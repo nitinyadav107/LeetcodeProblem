28bd858362d8930dfcc4cac001e555b54e13f71a
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2025-05-16 11:26:05 +0530

    Time: 154 ms (5.12%), Space: 55.4 MB (7.21%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,21 @@
+class Solution {
+    int [][]dp=new int[501][501];
+    int helper(String word1,String word2,int n1,int n2,int i1,int i2){
+        if(i1==n1) return n2-i2;
+        if(i2==n2) return n1-i1;
+        if(dp[i1][i2]!=-1) return dp[i1][i2];
+        if(word1.charAt(i1)==word2.charAt(i2)) return helper(word1,word2,n1,n2,i1+1,i2+1);
+        int insert=1+helper(word1,word2,n1,n2,i1,i2+1);
+        int delete=1+helper(word1,word2,n1,n2,i1+1,i2);
+        int replace=1+helper(word1,word2,n1,n2,i1+1,i2+1);
+        return dp[i1][i2]=Math.min( Math.min(insert,delete),replace);
+    }
+    public int minDistance(String word1, String word2) {
+        int n1=word1.length();
+        int n2=word2.length();
+        for(int i=0;i<501;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return helper(word1,word2,n1,n2,0,0);
+    }
+}
