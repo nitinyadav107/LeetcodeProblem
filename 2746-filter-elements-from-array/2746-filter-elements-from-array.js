d64358e2cb887c77f48d6dba24308e9791265562
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2025-08-05 23:21:09 +0530

    Time: 38 ms (77.83%), Space: 54 MB (27.07%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -4,7 +4,14 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-     let ans=arr.filter(fn);
-     return ans;
+    //  let ans=arr.filter(fn);
+    //  return ans;
+    let ans=[];
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i,arr)){
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
 
 };
